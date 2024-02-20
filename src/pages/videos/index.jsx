2908df5224e09abf73768fa7512d8a7a7599ceb4
moelshenawy <mohamedgk5@gmail.com>
2024-02-21 00:59:47 +0200

    ..

--- src/pages/videos/index.jsx
+++ src/pages/videos/index.jsx
@@ -73,9 +73,8 @@ const Videos = () => {
               <iframe
                 width="560"
                 height="315"
-                src={video.url}
+                src={video.url + "?modestbranding=1&rel=0"}
                 title="YouTube video player"
-                frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 allowFullScreen
               ></iframe>
