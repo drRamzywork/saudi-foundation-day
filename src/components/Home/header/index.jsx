2b8836b05640bdfd06ce7726e93548d38e50e87e
moelshenawy <mohamedgk5@gmail.com>
2024-02-21 06:13:32 +0200

    ..

--- src/components/Home/header/index.jsx
+++ src/components/Home/header/index.jsx
@@ -10,51 +10,56 @@ const Header = () => {
 
 
   return (
-    <header id='header' className={styles.header}>
-      <div className="container">
-        <motion.div
-          initial={{ opacity: 0, x: -100 }}
-          animate={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 1 }} className={styles.logo_container}>
-          <Link href={'/'} className={styles.logo}>
-            {router.pathname === '/' ?
+    <>
 
-              <Image width={528} height={452} src={'/assets/svgs/logo.svg'} />
-              :
-              <Image width={528} height={452} src={'/assets/svgs/logo_dark.svg'} />
-            }
-
-          </Link>
-
-        </motion.div>
-
-        {router.pathname === '/' &&
+      <header id='header' className={styles.header}>
+        <div className="container">
           <motion.div
-            initial={{ opacity: 0, x: 100 }}
+            initial={{ opacity: 0, x: -100 }}
             animate={{ opacity: 1, x: 0 }}
             exit={{ opacity: 0 }}
-            transition={{ duration: 1 }}
-            className={styles.languages_container}>
-            <a href={`/ar/videos`} className={styles.lang} locale="ar" passHref>
-              <Image width={50} height={50} src={'/assets/svgs/ar_flag.svg'} />
-            </a>
-            <a href={`/en/videos`} className={styles.lang} locale="en" passHref>
-              <Image width={50} height={50} src={'/assets/svgs/en_flag.svg'} />
-            </a>
-            <a href={`/fr/videos`} className={styles.lang} locale="fr" passHref>
-              <Image width={50} height={50} src={'/assets/svgs/fr_flag.svg'} />
-            </a>
-            <a href={`/es/videos`} className={styles.lang} locale="es" passHref>
-              <Image width={50} height={50} src={'/assets/svgs/es_flag.svg'} />
-            </a>
-            <a href={`/ru/videos`} className={styles.lang} locale="ru" passHref>
-              <Image width={50} height={50} src={'/assets/svgs/ru_flag.svg'} />
-            </a>
+            transition={{ duration: 1 }} className={styles.logo_container}>
+            <Link href={'/'} className={styles.logo}>
+              {router.pathname === '/' ?
+                <Image width={528} height={452} src={'/assets/svgs/logo.svg'} />
+                :
+                <Image width={528} height={452} src={'/assets/svgs/logo_dark.svg'} />
+              }
+
+            </Link>
+
           </motion.div>
-        }
-      </div>
-    </header>
+
+
+        </div>
+      </header>
+
+      {router.pathname === '/' &&
+        <motion.div
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 1 }}
+          className={styles.languages_container}>
+          <a href={`/ar/videos`} className={styles.lang} locale="ar" passHref>
+            <Image width={50} height={50} src={'/assets/svgs/ar_flag.svg'} />
+          </a>
+          <a href={`/en/videos`} className={styles.lang} locale="en" passHref>
+            <Image width={50} height={50} src={'/assets/svgs/en_flag.svg'} />
+          </a>
+          <a href={`/fr/videos`} className={styles.lang} locale="fr" passHref>
+            <Image width={50} height={50} src={'/assets/svgs/fr_flag.svg'} />
+          </a>
+          <a href={`/es/videos`} className={styles.lang} locale="es" passHref>
+            <Image width={50} height={50} src={'/assets/svgs/es_flag.svg'} />
+          </a>
+          <a href={`/ru/videos`} className={styles.lang} locale="ru" passHref>
+            <Image width={50} height={50} src={'/assets/svgs/ru_flag.svg'} />
+          </a>
+        </motion.div>
+      }
+
+    </>
   )
 }
 
